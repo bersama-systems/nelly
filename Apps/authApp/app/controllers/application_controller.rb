2700aebc2a5efc9f802a142ff1bcaa9b30a1dc3b
Joshua Teitelbaum <joshuat@gmail.com>
2024-01-26 15:36:39 -0800

    add auth

--- Apps/authApp/app/controllers/application_controller.rb
+++ Apps/authApp/app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   respond_to? :json
+
+  def is_authed?
+    !request&.headers["AuthApp-SessionId"].blank? && request&.headers["AuthApp-SessionId"].starts_with?("lolsecurity")
+  end
 end
--- Apps/authApp/app/controllers/users_controller.rb
+++ Apps/authApp/app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 
 class UsersController < ApplicationController
+
+  before_action :check_auth, only: [:index, :show]
+
+  def check_auth
+    if !is_authed?
+      render status: :unauthorized, json: { error: "You are not authorized to access this resource. Verify that you are passing passing your token."}
+      return false
+    end
+    return true
+
+  end
   def index
     @users = User.all
     render json: @users
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    render json: @user
   end
 end
 
