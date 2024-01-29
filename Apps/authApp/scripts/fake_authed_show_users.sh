d032549c3d67e33ca5f831788643bd305ecc675f
Joshua Teitelbaum <joshuat@gmail.com>
2024-01-28 18:33:35 -0800

    dockerfile

--- Apps/authApp/scripts/fake_authed_show_users.sh
+++ Apps/authApp/scripts/fake_authed_show_users.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 account=$1
-curl -H "AuthApp-AccountId: $account" -H "AuthApp-SessionId: lolsecurity" http://localhost:3000/users
+#curl -H "AuthApp-AccountId: $account" -H "AuthApp-SessionId: lolsecurity" http://localhost:3000/users
+curl -H "AuthApp-AccountId: $account" -H "AuthApp-SessionId: lolsecurity" http://127.0.0.1:3000/users
