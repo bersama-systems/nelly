1c7bb0da5dcea831f468b169c76eb1988bbf934c
Joshua Teitelbaum <joshuat@gmail.com>
2024-11-30 23:19:55 -0800

    clean

---
+++ src/clean.sh
@@ -0,0 +1,12 @@
+echo "running cleanup!!!"
+docker stop src-nginx-1
+docker rm src-nginx-1
+docker rmi src-nginx
+
+docker stop src-nodejs-1
+docker rm src-nodejs-1
+docker rmi src-nodejs
+
+docker stop src-redis-1
+docker rm src-redis-1
+docker rmi src-redis
