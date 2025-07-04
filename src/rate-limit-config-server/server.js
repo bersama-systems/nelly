f5e42bfe567e1cc96717fd07734a33125c05e6bc
Joshua Teitelbaum <joshuat@gmail.com>
2025-07-04 09:28:14 -0600

    3008 port

--- src/rate-limit-config-server/server.js
+++ src/rate-limit-config-server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3008;
 const CONFIG_FILE = 'limits.json';
 
 // Middleware
